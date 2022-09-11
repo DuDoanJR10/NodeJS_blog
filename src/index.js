b06e97419ab8e4d492c8390c5898ecaa1e859222
DuDoanJR10 <dudoanjr10@gmail.com>
2022-09-11 15:31:18 +0700

    Config Prettier

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
-        app.listen(port, () => {
-            console.log(`App listening at: http://localhost:${port}`);
-        });
+app.listen(port, () => {
+    console.log(`App listening at: http://localhost:${port}`);
+});
